1308d0d08685447825c66401bcfdd9c41a36f619
Sergei Nosov <sergei.nosov@gmail.com>
2013-06-28 19:06:21 +0400

    added cartesian product for array of arrays

--- experiments.d
+++ experiments.d
@@ -7,15 +7,19 @@ import std.range;
 import std.process;
 import std.algorithm;
 import std.regex;
+import std.datetime;
 
 struct CartesianProduct(Args...)
+    if (Args.length > 1)
 {
     private Args ranges_;
     private Args current_;
 
     alias staticMap!(ElementType, Args) ElementTypes;
+
 public:
-    this(Args ranges) {
+    this(Args ranges)
+    {
         foreach (i, ref r; ranges)
         {
             ranges_[i] = r.save;
@@ -23,15 +27,16 @@ public:
         }
     }
 
-    @property auto front() {
+    @property auto front()
+    {
         Tuple!ElementTypes ret;
-        foreach (i, r; current_) {
+        foreach (i, r; current_)
             ret[i] = r.front;
-        }
         return ret;
     }
 
-    @property void popFront() {
+    @property void popFront()
+    {
         foreach (i, ref r; current_)
         {
             r.popFront;
@@ -42,12 +47,14 @@ public:
         }
     }
 
-    @property bool empty() {
+    @property bool empty()
+    {
         return current_[$-1].empty;
     }
 }
 
 auto cartesianProduct(Args...)(Args args)
+if (Args.length > 1)
 {
     return CartesianProduct!(Args)(args);
 }
@@ -84,24 +91,183 @@ unittest {
                        Tuple!(int, double, string)(1, 3.3, "c"),
                        Tuple!(int, double, string)(2, 3.3, "c")]
                ));
-        // auto m = match("hel%(lo)qq%(zxcv)q", regex(`%\(.*?\)`, "g"));
+    // auto m = match("hel%(lo)qq%(zxcv)q", regex(`%\(.*?\)`, "g"));
     // if (!m.empty)
     //     writeln(m.pre, " | ", m.hit, " | ", m.post);
 }
 
-struct InputPatch
+struct CartesianProduct(T)
 {
-    private string patch_;
-    private string current_;
+    private T[][] ranges_;
+    private int[] current_;
 
-    this(string patch) {
-        patch_ = patch;
+public:
+    this(T[][] ranges)
+    {
+        ranges_ = ranges;
+        current_.length = ranges.length;
+    }
 
-        for (;;)
+    @property auto front()
+    {
+        T[] ret;
+        ret.length = ranges_.length;
+
+        foreach(i, r; ranges_)
+            ret[i] = r[current_[i]];
+
+        return ret;
+    }
+
+    @property void popFront()
+    {
+        foreach (i, r; ranges_)
         {
-
+            if ((++current_[i]) == ranges_[i].length && i != current_.length - 1)
+                current_[i] = 0;
+            else
+                break;
         }
     }
+
+    @property bool empty()
+    {
+        return current_[$-1] == ranges_[$-1].length;
+    }
+}
+
+auto cartesianProduct(T)(T[][] arg)
+{
+    return CartesianProduct!T(arg);
+}
+
+unittest {
+    auto a1 = ["1", "2"];
+    auto a2 = ["a", "b", "c"];
+    assert(equal(
+               [["1", "a"], ["2", "a"], ["1", "b"], ["2", "b"], ["1", "c"], ["2", "c"]],
+               CartesianProduct!string([a1, a2])));
+
+    // same, to test a1 and a2 are not modified
+    assert(equal(
+               [["1", "a"], ["2", "a"], ["1", "b"], ["2", "b"], ["1", "c"], ["2", "c"]],
+               CartesianProduct!string([a1, a2])));
+
+    auto cp2 = CartesianProduct!string([["1", "2"], ["1.1", "2.2", "3.3"], ["a", "b", "c"]]);
+    assert(equal(cp2, [["1", "1.1", "a"],
+                       ["2", "1.1", "a"],
+                       ["1", "2.2", "a"],
+                       ["2", "2.2", "a"],
+                       ["1", "3.3", "a"],
+                       ["2", "3.3", "a"],
+                       ["1", "1.1", "b"],
+                       ["2", "1.1", "b"],
+                       ["1", "2.2", "b"],
+                       ["2", "2.2", "b"],
+                       ["1", "3.3", "b"],
+                       ["2", "3.3", "b"],
+                       ["1", "1.1", "c"],
+                       ["2", "1.1", "c"],
+                       ["1", "2.2", "c"],
+                       ["2", "2.2", "c"],
+                       ["1", "3.3", "c"],
+                       ["2", "3.3", "c"]]
+               ));
+
+
+    assert(equal([[1, 3], [2, 3], [1, 4], [2, 4], [1, 5], [2, 5]],
+                 cartesianProduct([[1, 2], [3, 4, 5]])));
+}
+
+/**
+   Splits the patch extracting macros
+
+   Returns: array with the following structure: [, macro, post]
+ */
+// string[] splitMacros(string patch, string open_delim = "($", string close_delim = ")")
+// {
+
+// }
+
+
+/**
+   int <end>
+   int <begin> <end>
+   int <begin> <end> <step>
+   double <begin> <end> <number>
+   enum <value> <value> ...
+ */
+
+struct StepMacro(T)
+    if (
+        is(typeof(T.init + T.init)) &&
+        is(typeof(T.init >= T.init) == bool)
+        )
+{
+private:
+    T begin_;
+    T end_;
+    T step_;
+public:
+    this(T begin, T end, T step)
+    in
+    {
+        assert((step > 0) && (begin <= end) ||
+               (step < 0) && (begin >= end));
+    }
+    body
+    {
+        begin_ = begin;
+        end_   = end;
+        step_  = step;
+    }
+
+    @property T front() { return begin_; }
+    @property void popFront() { begin_ += step_; }
+    @property bool empty() { return
+            (step_ > 0) && (begin_ >= end_) ||
+            (step_ < 0) && (begin_ <= end_);
+    }
+}
+
+unittest {
+    assert(equal(StepMacro!int(0, 10, 1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+    assert(equal(StepMacro!int(3, 10, 2), [3, 5, 7, 9]));
+    assert(equal(StepMacro!long(4, 10, 2), [4, 6, 8]));
+    assert(equal(StepMacro!byte(10, -3, -3), [10, 7, 4, 1, -2]));
+}
+
+struct Macro
+{
+public:
+    this(string m)
+    {
+        auto splitted = m
+            .splitter(' ')
+            .filter!(s => !s.empty)
+            .array;
+
+        switch (splitted[0]) {
+        case "int":
+            break;
+        default:
+            throw new Exception("Unrecognized macro");
+        }
+    }
+}
+
+unittest {
+    auto m = Macro("int 3 3 1");
+
+    // auto m = match("hel%(lo)qq%(zxcv)q", regex(`%\(.*?\)`, "g"));
+    // if (!m.empty)
+    //     writeln(m.pre, " | ", m.hit, " | ", m.post);
+    // foreach (m; match("hel%(lo)qq%(zxcv)q", regex(`%\(.*?\)`, "g")))
+    //     writeln(m.pre, " | ", m.hit, " | ", m.post);
+
+
+
+//    writeln(splitter("hello  world", " "));
 }
 
 void main(string args[])
